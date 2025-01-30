f1558c35e145e0a8d8f1d9c2ded2519ed736f267
Gitfoe <wafoe@hotmail.com>
2025-01-30 14:33:30 +0800

    Sample channels list with Bootstrap

---
+++ react-client/src/components/ListGroup.tsx
@@ -0,0 +1,16 @@
+function ListGroup() {
+  return (
+    <>
+      <h1>Channels List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">Channel 1</li>
+        <li className="list-group-item">Channel 2</li>
+        <li className="list-group-item">Channel 3</li>
+        <li className="list-group-item">Channel 4</li>
+        <li className="list-group-item">Channel 5</li>
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
