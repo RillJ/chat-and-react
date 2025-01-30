0966244cd1a8d8da76ce7c6688dc5f38594f219e
Gitfoe <wafoe@hotmail.com>
2025-01-30 15:49:21 +0800

    Selectable channels

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,7 +1,16 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup /></div>;
+  const items = ["Channel 1", "Channel 2", "Channel 3", "Channel 4"];
+  const handleSelectChannel = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup items={items} onSelectChannel={handleSelectChannel} />
+    </div>
+  );
 }
 
 export default App;
--- react-client/src/components/ListGroup.tsx
+++ react-client/src/components/ListGroup.tsx
@@ -1,13 +1,40 @@
-function ListGroup() {
+import { useState } from "react";
+
+interface ListGroupProps {
+  items: string[];
+  onSelectChannel: (item: string) => void;
+}
+
+function ListGroup({ items, onSelectChannel }: ListGroupProps) {
+  // items = [];
+  const [selectedIndex, setSelectedIndex] = useState(-1); // Hook
+
+  //   const getChannel = () => {
+  //     return items.length === 0 ? <p>No channels found.</p> : null;
+  //   }
+
   return (
     <>
       <h1>Channels List</h1>
+      {items.length === 0 && <p>No channels found.</p>}
       <ul className="list-group">
-        <li className="list-group-item">Channel 1</li>
-        <li className="list-group-item">Channel 2</li>
-        <li className="list-group-item">Channel 3</li>
-        <li className="list-group-item">Channel 4</li>
-        <li className="list-group-item">Channel 5</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectChannel(item);
+            }}
+          >
+            {/* Probably use item.id from API */}
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
