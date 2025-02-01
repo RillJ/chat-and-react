c499a333db6d4d31227a4cc406621caa82fa7146
Gitfoe <wafoe@hotmail.com>
2025-02-01 17:04:07 +0800

    Apache license headers

--- channel.py
+++ channel.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Julian Calvin Rill
+# Copyright 2025 Julian Calvin Rill
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
